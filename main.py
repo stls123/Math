451cdfd868c409072ee8b1c415d1df395bfb5776
stls123 <1442034181@qq.com>
2024-08-02 12:08:40 +0800

    添加了.idea文件夹，用于存储IDEA配置文件

---
+++ main.py
@@ -0,0 +1,19 @@
+"""
+如果是第⼀次使⽤ Git，你需要设置署名和邮箱：
+    $ git config--global user.name "⽤户名"
+    $ git config--global user.email "电⼦邮箱"
+"""
+# 将代码clone到本地
+# git clone https://github.com/zhongqiangwu/ChatGPT-WebUI.git  # ⽤户名/仓库名
+# git clone git@github.com:someone/symfony-docs-chs.git  # SSH⽅式
+"""
+向这个本地的代码仓库添加当前⽬录的所有改动：
+$ git add .
+
+者只是添加某个⽂件：
+$ git add-p
+
+我们可以输⼊
+$git status
+"""
+print("hello world")
